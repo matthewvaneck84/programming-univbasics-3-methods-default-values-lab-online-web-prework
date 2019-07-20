cb852d1fd23ed11e1710529132e89bb6b47f897e
Matthew Van Eck <thematt84@yahoo.com>
2019-07-20 12:11:54 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,1 +1,17 @@
-# Your code here
+def meal_choice(veg1, veg2, protein="meat")
+  puts "What a nutritious meal!"
+  puts "A plate of #{protein} with #{veg1} and #{veg2}."
+  
+end 
+  
+  
+  def meal_choice(veg1, veg2, protein)
+    return "A plate of #{protein} with #{veg1} and #{veg2}."
+  end 
+  
+  
+  veg1 = "brocolli"
+  veg2 = "macaroni"
+  protein = "tofu"
+  
+meal_choice
